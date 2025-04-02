4aa3b28a055d801ee2f86522b7eabf8e9c378222
SunYukey <syj@boditech.co.kr>
2025-04-02 16:45:43 +0800

    A的第四次提交

--- main.py
+++ main.py
@@ -9,3 +9,11 @@ def sum_two_smallest_numbers(numbers):
     numbers.sort()
     return numbers[0] + numbers[1]
 
+def likes(names):
+    if len(names) == 0:
+        return "no one likes this"
+    elif len(names) == 1:
+        return f"{names[0]} likes this"
+    elif len(names) == 2:
+        return f"{names[0]} and {names[1]} like this"
+    elif len(names) == 3:
